bf085ab604e983a08c0af0e8fb10d35fbac6334e
zhang <zh2362207401@gmail.com>
2024-08-27 14:07:26 +0800

    首次上传

---
+++ simulation_all/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd src/
+
+rm CMakeLists.txt
+
+source /opt/ros/noetic/setup.bash
+
+catkin_init_workspace
+
+cd ..
+
+catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3
---
+++ simulation_all/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source./devel/setup.bash
+
+roslaunch simulation_all gazebo_qr.launch
+
