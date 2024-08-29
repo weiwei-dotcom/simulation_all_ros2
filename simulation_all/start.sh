58706063b22705a0b31983c73efca1a51f25c84d
zhang <zh2362207401@gmail.com>
2024-08-29 14:00:21 +0800

    增加了启动脚本，当前支持启动宇树go1、a1，合作机械臂，傅里叶智能gr1,

--- simulation_all/start.sh
+++ simulation_all/start.sh
@@ -2,5 +2,20 @@
 
 source devel/setup.bash
 
-roslaunch simulation_all gazebo_qr.launch
-
+if [ $# == 1 ];then
+    if [ "$1" == 'arm' ]; then
+        roslaunch simulation_all gazebo_arm.launch
+    elif [ "$1" == 'qr' ]; then
+        roslaunch simulation_all gazebo_qr.launch
+    elif [ "$1" == 'gh' ]; then
+        roslaunch simulation_all gazebo_gh.launch
+fi
+elif [ $# == 2 ]; then
+    if [ "$1" == 'qr' ]; then
+            roslaunch simulation_all gazebo_qr.launch rname:=$2
+        elif [ "$1" == 'gh' ]; then
+            roslaunch simulation_all gazebo_gh.launch rname:=$2
+    fi
+else
+    echo "please input bootargs!!"
+fi
