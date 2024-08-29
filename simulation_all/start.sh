02be2327356dc295888fe0945becf44a822c0c39
zhang <zh2362207401@gmail.com>
2024-08-29 14:24:45 +0800

    增加了wl小型四足 修改了启动脚本 例：./start.sh qr(arm/human) go1(wl) user(debug)

--- simulation_all/start.sh
+++ simulation_all/start.sh
@@ -7,14 +7,22 @@ if [ $# == 1 ];then
         roslaunch simulation_all gazebo_arm.launch
     elif [ "$1" == 'qr' ]; then
         roslaunch simulation_all gazebo_qr.launch
-    elif [ "$1" == 'gh' ]; then
+    elif [ "$1" == 'human' ]; then
         roslaunch simulation_all gazebo_gh.launch
-fi
+    fi
 elif [ $# == 2 ]; then
     if [ "$1" == 'qr' ]; then
             roslaunch simulation_all gazebo_qr.launch rname:=$2
         elif [ "$1" == 'gh' ]; then
             roslaunch simulation_all gazebo_gh.launch rname:=$2
+    fi
+elif [ $# == 3 ]; then
+    if [ "$1" == 'qr' ]; then
+        if [ "$3" == 'user' ]; then
+            roslaunch simulation_all gazebo_qr.launch rname:=$2
+        elif [ "$3" == 'debug' ]; then
+            roslaunch simulation_all gazebo_qr.launch rname:=$2 user_debug:=true
+        fi
     fi
 else
     echo "please input bootargs!!"
