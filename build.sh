3b49e5b0d24ed3dd8f69e6ffdb391791a597183d
weiwei-dotcom <728841808@qq.com>
2024-12-09 10:29:59 +0800

    .添加qa01仿真环境

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -r install/ build/
+# rm -r install/ build/
 
 source /opt/ros/foxy/setup.bash
 
-colcon build --packages-select simulation_all_ros2
+colcon build
--- start.sh
+++ start.sh
@@ -1,37 +1,15 @@
-#!/bin/bash
-
 source install/setup.bash
 
-if [ $# == 1 ];then
-    if [ "$1" == 'arm' ]; then
-        ros2 launch simulation_all_ros2 gazebo_arm.launch.py
-    elif [ "$1" == 'qr' ]; then
-        ros2 launch simulation_all_ros2 gazebo_qr.launch.py
-    elif [ "$1" == 'human' ]; then
-        ros2 launch simulation_all_ros2 gazebo_gh.launch.py
-    fi
-elif [ $# == 2 ]; then
-    if [ "$1" == 'qr' ]; then
-        ros2 launch simulation_all_ros2 gazebo_qr.launch.py rname:=$2
-    elif [ "$1" == 'human' ]; then
-        ros2 launch simulation_all_ros2 gazebo_gh.launch.py rname:=$2
-    elif [ "$1" == 'arm' ]; then
-        ros2 launch simulation_all_ros2 gazebo_arm.launch.py rname:=$2
-    fi
-elif [ $# == 3 ]; then
-    if [ "$1" == 'qr' ]; then
-        if [ "$3" == 'user' ]; then
-            ros2 launch simulation_all_ros2 gazebo_qr.launch.py rname:=$2
-        elif [ "$3" == 'debug' ]; then
-            ros2 launch simulation_all_ros2 gazebo_qr.launch.py rname:=$2 user_debug:=true
-        fi
-    elif [ "$1" == 'human' ]; then
-        if [ "$3" == 'user' ]; then
-            ros2 launch simulation_all_ros2 gazebo_gh.launch.py rname:=$2
-        elif [ "$3" == 'debug' ]; then
-            ros2 launch simulation_all_ros2 gazebo_gh.launch.py rname:=$2 user_debug:=true
-        fi
-    fi
+if [ "$1" == 'gh1' ] || [ "$1" == 'gh' ]; then
+    ros2 launch simulation_all_ros2 gazebo_gh1.launch.py
+elif [ "$1" == 'sz' ]; then
+    ros2 launch simulation_all_ros2 gazebo_sz.launch.py
+elif [ "$1" == 'mz' ]; then
+    ros2 launch simulation_all_ros2 gazebo_mz.launch.py
+elif [ "$1" == 'iris' ]; then
+    ros2 launch simulation_all_ros2 gazebo_iris.launch.py
+elif [ "$1" == 'qa01' ]; then
+    ros2 launch simulation_all_ros2 gazebo_qa01.launch.py
 else
-    echo "please input bootargs!!"
+    echo "please input the correct name of robot !"
 fi
